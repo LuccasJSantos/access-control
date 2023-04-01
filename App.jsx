12939ed309851518551fb5e1ddac1ce699b3ca81
Luccas Santos <luccas_josival_lab2dev@whirlpool.com>
2023-04-01 03:12:18 -0300

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import { NativeBaseProvider, Button } from "native-base"
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Button>Test</Button>
+    </NativeBaseProvider>
+  )
+}
