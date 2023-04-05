df411d699e8aa79b012d3afb12f7736605825bbc
Luccas Santos <luccas_josival_lab2dev@whirlpool.com>
2023-04-05 02:27:51 -0300

    Add route config

--- App.jsx
+++ App.jsx
@@ -1,9 +1,22 @@
-import { NativeBaseProvider, Button } from "native-base"
+import { NativeBaseProvider } from "native-base"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+import Login from "./src/screens/Login"
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Button>Test</Button>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NativeBaseProvider>
+    </NavigationContainer>
   )
 }
