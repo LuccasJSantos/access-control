3941429f7f02773b73f8e227614eeab75e3837a0
luccasjsantos <luccasjsantos@hotmail.com>
2023-06-17 20:22:24 -0300

    feat: add List component and hook it to the Home page

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,22 +1,40 @@
 import { Box, Divider, FlatList } from 'native-base'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 
-const List = ({ items, render }) => {
-  return <Box rounded="lg" className="bg-white px-1.5">
-    <FlatList
-      data={items}
-      renderItem={({ item, index }) => {
-        return (
-          <View>
-            {render(item, index, items)}
-            {index < items.length - 1 && (
-              <Divider className="bg-gray-100" />
-            )}
-          </View>
-        )
-      }}
-    />
-  </Box>
+const List = ({ items, render, footerText }) => {
+  const NoData = () => (
+    <Text className="text-center font-medium py-4 text-gray-500">Sem dados no momento</Text>
+  )
+
+  return <View>
+    <Box rounded="lg" className="bg-white px-1.5">
+      {
+      !items.length
+        ? <NoData />
+        : <FlatList
+          data={items}
+          renderItem={({ item, index }) => {
+            return (
+              <View key={index}>
+                {render(item, index, items)}
+                {index < items.length - 1 && (
+                  <Divider className="bg-gray-100" />
+                )}
+              </View>
+            )
+          }}
+        />
+      }
+    </Box>
+    {
+      footerText &&
+      <View className="flex-row w-full justify-end pt-1">
+        <Text className="flex text-gray-500 text-xs text-right">
+          {footerText}
+        </Text>
+      </View>
+    }
+  </View>
 }
 
 export default List
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -41,6 +41,7 @@ function Home () {
       className="absolute -z-10 top-0 left-0 w-full h-full py-3 px-5"
     >
       <SafeAreaView>
+        {/* Header */}
         <View className="flex-row items-center justify-between w-full">
           <Text className="text-xl font-bold">
             Bom dia,
@@ -49,6 +50,8 @@ function Home () {
 
           <Avatar className="bg-accent">{avatarText}</Avatar>
         </View>
+
+        {/* Connection section */}
         <Section
           title="Conexão"
           link={!connected && { title: 'Configurar', screen: 'connection-settings' }}
@@ -66,40 +69,37 @@ function Home () {
           }
         </Section>
 
+        {/* Recent access section */}
         <Section
           title="Acessos recentes"
-          link={{ title: 'Ver todos', screen: 'Access' }}
+          link={data.length && { title: 'Ver todos', screen: 'Access' }}
           className="mt-5"
         >
-          <List items={data} render={(item, index) => (
-            <View>
-              <View className="flex-row items-center justify-between w-full py-3 px-2">
-                <View className="gap-0.5">
-                  <Text className="text-xs font-semibold">
-                    {item.title}
-                  </Text>
+          <List
+            items={data}
+            footerText={!connected && 'Conecte-se para atualizar a lista'}
+            render={item => (
+              <View>
+                <View className="flex-row items-center justify-between w-full py-3 px-2">
+                  <View className="gap-0.5">
+                    <Text className="text-xs font-semibold">
+                      {item.title}
+                    </Text>
+                    <Text className="text-xs text-gray-400">
+                      {item.subtitle}
+                    </Text>
+                  </View>
+
                   <Text className="text-xs text-gray-400">
-                    {item.subtitle}
+                    {item.detail.toFormat('dd/MM/yy')} •{' '}
+                    {item.detail.toFormat('HH:mm')}
                   </Text>
                 </View>
-
-                <Text className="text-xs text-gray-400">
-                  {item.detail.toFormat('dd/MM/yy')} •{' '}
-                  {item.detail.toFormat('HH:mm')}
-                </Text>
               </View>
-            </View>
-          )} />
-
-          <View className="flex-row w-full justify-end pt-1">
-            { !connected &&
-              <Text className="flex text-gray-500 text-xs text-right">
-                Conecte-se para atualizar a lista
-              </Text>
-            }
-          </View>
+            )} />
         </Section>
 
+        {/* Users section */}
         <Section
           title="Usuários"
           link={{ title: 'Ver todos', screen: 'Access' }}
@@ -107,7 +107,8 @@ function Home () {
         >
           <List
             items={data}
-            render={(item) => {
+            footerText={!connected && 'Conecte-se para atualizar a lista'}
+            render={item => {
               return (
                 <View>
                   <View className="flex-row items-center justify-between w-full py-3 px-2">
@@ -129,14 +130,6 @@ function Home () {
               )
             }}
           />
-
-          <View className="flex-row w-full justify-end pt-1">
-            { !connected &&
-              <Text className="flex text-gray-500 text-xs text-right">
-                Conecte-se para atualizar a lista
-              </Text>
-            }
-          </View>
         </Section>
       </SafeAreaView>
     </LinearGradient>
