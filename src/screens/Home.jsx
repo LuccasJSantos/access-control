7aa316b7b9d7c36837f34d1a4b649b2c5b2bf075
luccasjsantos <luccasjsantos@hotmail.com>
2023-06-18 00:10:21 -0300

    feat: add refresh control to reload data by swiping from the top

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,8 @@
 import { Avatar, Text, Toast } from 'native-base'
-import { SafeAreaView, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView, TouchableOpacity, View, FlatList, RefreshControl } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import Section from '../components/Section'
 import ConnectionWidget from '../components/ConnectionWidget'
@@ -18,14 +18,22 @@ function Home () {
   const { access, init: accessInit } = useAccess()
   const { users, init: usersInit } = useUsers()
   const navigation = useNavigation()
+  const [refreshing, setRefreshing] = useState(false)
 
   const [firstname] = username.split(' ')
   const [lastname] = username.split(' ').slice(-1)
   const avatarText = `${firstname[0]}${lastname[0]}`
 
+  function loadData () {
+    setRefreshing(true)
+    Promise.all([
+      accessInit().catch(error => Toast.show({ description: error.message, duration: 3000 })),
+      usersInit().catch(error => Toast.show({ description: error.message, duration: 3000 }))
+    ]).finally(() => setRefreshing(false))
+  }
+
   useEffect(() => {
-    accessInit().catch(error => Toast.show({ description: error.message, duration: 3000 }))
-    usersInit().catch(error => Toast.show({ description: error.message, duration: 3000 }))
+    loadData()
   }, [])
 
   return (
@@ -35,96 +43,103 @@ function Home () {
       className="absolute -z-10 top-0 left-0 w-full h-full py-3 px-5"
     >
       <SafeAreaView>
-        {/* Header */}
-        <View className="flex-row items-center justify-between w-full">
-          <Text className="text-xl font-bold">
-            Bom dia,
-            <Text className="ml-1 font-normal text-gray-400"> {firstname}</Text>
-          </Text>
+        <FlatList
+          data={['']}
+          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={loadData} />}
+          renderItem={() =>
+            <View>
+              {/* Header */}
+              <View className="flex-row items-center justify-between w-full">
+                <Text className="text-xl font-bold">
+                  Bom dia,
+                  <Text className="ml-1 font-normal text-gray-400"> {firstname}</Text>
+                </Text>
 
-          <Avatar className="bg-accent">{avatarText}</Avatar>
-        </View>
+                <Avatar className="bg-accent">{avatarText}</Avatar>
+              </View>
 
-        {/* Connection section */}
-        <Section
-          title="Conexão"
-          link={!connected && { title: 'Configurar', screen: 'connection-settings' }}
-          className="mt-5"
-        >
-          <View className="flex-row items-center gap-1">
-            <ConnectionWidget />
-          </View>
-          { !connected &&
+              {/* Connection section */}
+              <Section
+                title="Conexão"
+                link={!connected && { title: 'Configurar', screen: 'connection-settings' }}
+                className="mt-5"
+              >
+                <View className="flex-row items-center gap-1">
+                  <ConnectionWidget />
+                </View>
+                { !connected &&
             <TouchableOpacity className="mt-0.5" onPress={() => navigation.navigate('connection-settings')}>
               <Text className="text-xs text-gray-400">
                 Clique aqui para iniciar configuração
               </Text>
             </TouchableOpacity>
-          }
-        </Section>
+                }
+              </Section>
 
-        {/* Recent access section */}
-        <Section
-          title={`Acessos recentes (${access.length})`}
-          link={access.slice(4).length && { title: 'Ver todos', screen: 'Access' }}
-          className="mt-5"
-        >
-          <List
-            items={access.slice(0, 4)}
-            footerText={!connected && 'Conecte-se para atualizar a lista'}
-            render={item => (
-              <View>
-                <View className="flex-row items-center justify-between w-full py-3 px-2">
-                  <View className="gap-0.5">
-                    <Text className="text-xs font-semibold">
-                      {item.name}
-                    </Text>
-                    <Text className="text-xs text-gray-400">
-                      {item.role}
-                    </Text>
-                  </View>
+              {/* Recent access section */}
+              <Section
+                title={`Acessos recentes (${access.length})`}
+                link={access.slice(4).length && { title: 'Ver todos', screen: 'Access' }}
+                className="mt-5"
+              >
+                <List
+                  items={access.slice(0, 4)}
+                  footerText={!connected && 'Conecte-se para atualizar a lista'}
+                  render={item => (
+                    <View>
+                      <View className="flex-row items-center justify-between w-full py-3 px-2">
+                        <View className="gap-0.5">
+                          <Text className="text-xs font-semibold">
+                            {item.name}
+                          </Text>
+                          <Text className="text-xs text-gray-400">
+                            {item.role}
+                          </Text>
+                        </View>
 
-                  <Text className="text-xs text-gray-400">
-                    {item.date.toFormat('dd/MM/yy')} •{' '}
-                    {item.date.toFormat('HH:mm')}
-                  </Text>
-                </View>
-              </View>
-            )} />
-        </Section>
-
-        {/* Users section */}
-        <Section
-          title={`Usuários (${users.length})`}
-          link={users.slice(4).length && { title: 'Ver todos', screen: 'Users' }}
-          className="mt-5"
-        >
-          <List
-            items={users.slice(0, 4)}
-            footerText={!connected && 'Conecte-se para atualizar a lista'}
-            render={item => {
-              return (
-                <View>
-                  <View className="flex-row items-center justify-between w-full py-3 px-2">
-                    <View className="gap-0.5">
-                      <Text className="text-xs font-semibold">
-                        {item.name}
-                      </Text>
-                      <Text className="text-xs text-gray-400">
-                        {item.role}
-                      </Text>
+                        <Text className="text-xs text-gray-400">
+                          {item.date.toFormat('dd/MM/yy')} •{' '}
+                          {item.date.toFormat('HH:mm')}
+                        </Text>
+                      </View>
                     </View>
+                  )} />
+              </Section>
 
-                    <Text className="text-xs text-gray-400">
-                      {item.date.toFormat('dd/MM/yy')} •{' '}
-                      {item.date.toFormat('HH:mm')}
-                    </Text>
-                  </View>
-                </View>
-              )
-            }}
-          />
-        </Section>
+              {/* Users section */}
+              <Section
+                title={`Usuários (${users.length})`}
+                link={users.slice(4).length && { title: 'Ver todos', screen: 'Users' }}
+                className="mt-5"
+              >
+                <List
+                  items={users.slice(0, 4)}
+                  footerText={!connected && 'Conecte-se para atualizar a lista'}
+                  render={item => {
+                    return (
+                      <View>
+                        <View className="flex-row items-center justify-between w-full py-3 px-2">
+                          <View className="gap-0.5">
+                            <Text className="text-xs font-semibold">
+                              {item.name}
+                            </Text>
+                            <Text className="text-xs text-gray-400">
+                              {item.role}
+                            </Text>
+                          </View>
+
+                          <Text className="text-xs text-gray-400">
+                            {item.date.toFormat('dd/MM/yy')} •{' '}
+                            {item.date.toFormat('HH:mm')}
+                          </Text>
+                        </View>
+                      </View>
+                    )
+                  }}
+                />
+              </Section>
+            </View>
+          } />
       </SafeAreaView>
     </LinearGradient>
   )
